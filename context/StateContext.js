cde26f3fb840cba8f15ce4d97cbc9e4511297daf
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-10-25 20:26:52 +0530

    Creation of Cart window and logic to add and remove items from cart. minor bugs related to addition of cart items resolved

--- context/StateContext.js
+++ context/StateContext.js
@@ -8,17 +8,17 @@ export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
 
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
+    const [totalPrice, setTotalPrice] = useState(0);
 
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
-        if (checkProductInCart) {
-            setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
-            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
+        if (checkProductInCart) {
             const updatedCartItems = cartItems.map((cartProduct) => {
                 if (cartProduct._id === product._id) return {
                     ...cartProduct,
@@ -41,7 +41,7 @@ export const StateContext = ({ children }) => {
     const decQty = () => {
         setQty((prevQty) => {
             if (prevQty - 1 < 1) return 1
-            else return prevQty - 1
+            else return (prevQty - 1)
         });
     }
 
@@ -55,7 +55,8 @@ export const StateContext = ({ children }) => {
                 qty,
                 incQty,
                 decQty,
-                onAdd
+                onAdd,
+                setShowCart
             }}
         >
             {children}
